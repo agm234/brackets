405cfdda11b9aa0fffd714b7c82b6c99e8d5098b
agm234 <79218134+agm234@users.noreply.github.com>
2021-03-10 23:37:10 +0300

    feat: add check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let keys = [];
+  let value = [];
+  let obj = {};
+  if (str.length % 2 === 1) {
+    return false;
+  }
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    keys.push(bracketsConfig[i][0]);
+    value.push(bracketsConfig[i][1]);
+    obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (keys.includes(str[i]) && value.includes(str[i])) {
+      if (stack[stack.length - 1] === str[i]) {
+        stack.pop();
+      } else {
+        stack.push(str[i])
+      }
+    }
+    else if (keys.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (value.includes(str[i])) {
+      if (obj[stack[stack.length - 1]] === str[i]) {
+        stack.pop();
+      } else {
+        stack.push('1')
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  }
+  else return false;
 }
